730db1fd677a4fc14dec0ca03959b94d09df7ff7
John Darrington <john@darrington.wattle.id.au>
2016-02-28 15:08:54 +0100

    Added makefile to build the efficient-sheet

---
+++ src/ui/gui/efficient-sheet.mk
@@ -0,0 +1,8 @@
+src_ui_gui_psppire_SOURCES += \
+	src/ui/gui/efficient-sheet/jmd-axis-model.c \
+	src/ui/gui/efficient-sheet/jmd-constraint.c \
+	src/ui/gui/efficient-sheet/jmd-sheet.c \
+	src/ui/gui/efficient-sheet/jmd-sheet-axis.c \
+	src/ui/gui/efficient-sheet/jmd-sheet-body.c \
+	src/ui/gui/efficient-sheet/jmd-sheet-single.c \
+	src/ui/gui/efficient-sheet/jmd-datum.c
