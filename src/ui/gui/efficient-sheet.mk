1adcef1524f532e87238790bc2a255e6707afce9
John Darrington <john@darrington.wattle.id.au>
2016-03-16 16:51:14 +0100

    Update build to reflect new file in sheet implementation

--- src/ui/gui/efficient-sheet.mk
+++ src/ui/gui/efficient-sheet.mk
@@ -5,4 +5,5 @@ src_ui_gui_psppire_SOURCES += \
 	src/ui/gui/efficient-sheet/jmd-sheet-axis.c \
 	src/ui/gui/efficient-sheet/jmd-sheet-body.c \
 	src/ui/gui/efficient-sheet/jmd-sheet-single.c \
-	src/ui/gui/efficient-sheet/jmd-datum.c
+	src/ui/gui/efficient-sheet/jmd-datum.c \
+	src/ui/gui/efficient-sheet/jmd-cell.c
