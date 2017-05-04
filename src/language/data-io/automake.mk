b2b5300cd5deace18d4277c58dcbac7993693863
John Darrington <john@darrington.wattle.id.au>
2017-05-04 17:25:49 +0200

    New module matrix-reader
    Added a new module to help read the files created by MATRIX DATA

--- src/language/data-io/automake.mk
+++ src/language/data-io/automake.mk
@@ -22,6 +22,8 @@ language_data_io_sources = \
 	src/language/data-io/print-space.c \
 	src/language/data-io/print.c \
 	src/language/data-io/matrix-data.c \
+	src/language/data-io/matrix-reader.c \
+	src/language/data-io/matrix-reader.h \
 	src/language/data-io/save-translate.c \
 	src/language/data-io/save.c \
 	src/language/data-io/trim.c \
