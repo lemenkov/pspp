lemenkov/pspp

commit e2039bc0694b89d26065899d9ed20f9bb42426f4

trim.c: avoid zero size xnrealloc memory allocation
Without the patch the regression fails on test

GET with /KEEP=ALL crashes -- uncompressed

when I compile with -fsanitize=address on MacOS. The reason
is that xnrealloc is called with a zero size allocation. I
simply avoid this, although it might be perfectly o.k. to do so.

src/language/data-io/trim.c
@@ -302,6 +302,8 @@ parse_dict_keep (struct lexer *lexer, struct dictionary *dict)
   dict_reorder_vars (dict, v, nv);
 
   /* Delete the remaining variables. */
+  if (dict_get_var_cnt (dict) == nv)
+    return true;
   v = xnrealloc (v, dict_get_var_cnt (dict) - nv, sizeof *v);
   for (i = nv; i < dict_get_var_cnt (dict); i++)
     v[i - nv] = dict_get_var (dict, i);

commit 0f2df94c967b8464d4c9ff72f8e2a14f48db8bbe

Fix memory leak introduced in commit e2039bc0694b89d26065899d9ed20f9bb42426f4

src/language/data-io/trim.c
@@ -303,7 +303,11 @@ parse_dict_keep (struct lexer *lexer, struct dictionary *dict)
 
   /* Delete the remaining variables. */
   if (dict_get_var_cnt (dict) == nv)
-    return true;
+    {
+      free (v);
+      return true;
+    }
+
   v = xnrealloc (v, dict_get_var_cnt (dict) - nv, sizeof *v);
   for (i = nv; i < dict_get_var_cnt (dict); i++)
     v[i - nv] = dict_get_var (dict, i);