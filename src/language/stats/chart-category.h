b7135f0fc3ce1f369c20cd6f3450f924aaa01e39
jdarrington <john@darrington.wattle.id.au>
2020-08-09 16:21:33 +0200

    src/language/stats/chart-category.h: Add #include directive

--- src/language/stats/chart-category.h
+++ src/language/stats/chart-category.h
@@ -19,6 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef BARCHART_DEF_H
 #define BARCHART_DEF_H 1
 
+#include <stdbool.h>
+
 struct ag_func
 {
   const char *name;
