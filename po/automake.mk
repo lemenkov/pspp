lemenkov/pspp

commit b703fcd74fda545a8b0691f180aa2424c17488c1

Fix build error when building --without-gui
Closes bug #59069.

po/automake.mk
@@ -63,7 +63,7 @@ ALL_TRANSLATABLE_FILES = \
 $(POTFILE): $(ALL_TRANSLATABLE_FILES) Makefile
 	@$(MKDIR_P) po
 	$(AM_V_GEN)$(XGETTEXT) $(XGETTEXT_OPTIONS) $(TRANSLATABLE_FILES) --language=C --keyword=_ --keyword=N_ -o $@,tmp
-	$(AM_V_at)$(XGETTEXT) $(XGETTEXT_OPTIONS) -j $(UI_FILES) --language=Glade -o $@,tmp
+	test -z "$(UI_FILES)" || $(AM_V_at)$(XGETTEXT) $(XGETTEXT_OPTIONS) -j $(UI_FILES) --language=Glade -o $@,tmp
 	$(AM_V_at)$(XGETTEXT) $(XGETTEXT_OPTIONS) -j src/ui/gui/org.fsf.pspp.metainfo.xml.in -o $@,tmp
 	$(AM_V_at)$(XGETTEXT) $(XGETTEXT_OPTIONS) -j src/ui/gui/org.fsf.pspp.desktop.in -o $@,tmp
 	$(AM_V_at)$(SED) -e '/^"POT-Creation-Date: .*/d' $@,tmp > $@

commit 7333265a305a9f1c7c8eee275467b27f4a8c4e9b

po: Fix placement of $(AM_V_at) in automake.mk.
It has to be at the beginning of the line because it is interpreted by
make, not by the shell.

Fixes: commit b703fcd74fda ("Fix build error when building --without-gui")

po/automake.mk
@@ -63,7 +63,7 @@ ALL_TRANSLATABLE_FILES = \
 $(POTFILE): $(ALL_TRANSLATABLE_FILES) Makefile
 	@$(MKDIR_P) po
 	$(AM_V_GEN)$(XGETTEXT) $(XGETTEXT_OPTIONS) $(TRANSLATABLE_FILES) --language=C --keyword=_ --keyword=N_ -o $@,tmp
-	test -z "$(UI_FILES)" || $(AM_V_at)$(XGETTEXT) $(XGETTEXT_OPTIONS) -j $(UI_FILES) --language=Glade -o $@,tmp
+	$(AM_V_at)test -z "$(UI_FILES)" || $(XGETTEXT) $(XGETTEXT_OPTIONS) -j $(UI_FILES) --language=Glade -o $@,tmp
 	$(AM_V_at)$(XGETTEXT) $(XGETTEXT_OPTIONS) -j src/ui/gui/org.fsf.pspp.metainfo.xml.in -o $@,tmp
 	$(AM_V_at)$(XGETTEXT) $(XGETTEXT_OPTIONS) -j src/ui/gui/org.fsf.pspp.desktop.in -o $@,tmp
 	$(AM_V_at)$(SED) -e '/^"POT-Creation-Date: .*/d' $@,tmp > $@