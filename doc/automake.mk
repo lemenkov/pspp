lemenkov/pspp

commit 8ec823cac8dd9eee5d0ad6cee9d001b9fdb673ba

Fix build of pspp.xml

Makefile.am
@@ -1,5 +1,5 @@
 ## PSPP - a program for statistical analysis.
-## Copyright (C) 2019 Free Software Foundation, Inc.
+## Copyright (C) 2019, 2020 Free Software Foundation, Inc.
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -65,6 +65,11 @@ UNINSTALL_DATA_HOOKS =
 PHONY =
 SUFFIXES = .q
 LDADD = gl/libgl.la
+
+gl/libgl.la: config.h
+	$(MAKE) -C gl
+
+dist: gl/libgl.la
 
 # This ensures that files added to EXTRA_DIST are always distributed,
 # even if they are inside an Automake if...endif conditional block that is

doc/automake.mk
@@ -78,7 +78,7 @@ $(srcdir)/doc/tut.texi:
 	$(AM_V_GEN)echo "@set example-dir $(examplesdir)" > $@
 
 
-$(srcdir)/doc/pspp.xml: doc/pspp.texi $(doc_pspp_TEXINFOS) $(top_srcdir)/doc/help-pages-list
+doc/pspp.xml: doc/pspp.texi $(doc_pspp_TEXINFOS) doc/help-pages-list
 if BROKEN_DOCBOOK_XML
 	touch $@
 else
@@ -91,13 +91,13 @@ docbookdir = $(docdir)
 dist_docbook_DATA = doc/pspp.xml
 
 
-CLEANFILES += pspp-dev.dvi $(docbook_DATA) doc/pspp.info*
+CLEANFILES += pspp-dev.dvi $(docbook_DATA) doc/pspp.info* doc/pspp.xml
 
 
 doc: $(INFO_DEPS) $(DVIS) $(PDFS) $(PSS) $(HTMLS) $(dist_docbook_DATA)
 PHONY += doc
 
-$(top_srcdir)/doc/help-pages-list: $(UI_FILES)
+doc/help-pages-list: $(UI_FILES)
 	 $(AM_V_GEN)cat $^ | grep '"help[-_]page"' | \
    $(SED) -e 's% *<property name="help[-_]page">\([^<]*\)</property>%//*[@id='"'"'\1'"'"']%' \
 	-e 's%#%'"'"']/*[@id='"'"'%g' > $@,tmp
@@ -140,6 +140,7 @@ $(top_builddir)/doc/pspp.ps:    $(EXAMPLE_OUTPUTS)
 $(top_builddir)/doc/pspp.dvi:   $(EXAMPLE_OUTPUTS)
 $(top_builddir)/doc/pspp.html:  $(EXAMPLE_HTML)
 $(top_builddir)/doc/pspp.pdf:   $(EXAMPLE_OUTPUTS)
+$(top_builddir)/doc/pspp.xml:   $(EXAMPLE_OUTPUTS)
 
 # The examples cannot be built until the binary has been built
 $(EXAMPLE_OUTPUTS): $(top_builddir)/src/ui/terminal/pspp

src/ui/gui/automake.mk
@@ -397,8 +397,6 @@ src/ui/gui/psppire-marshal.h: src/ui/gui/marshaller-list
 	$(AM_V_GEN)$(GLIB_GENMARSHAL) --header --prefix=psppire_marshal $? > $@
 
 mimedir = $(datadir)/mime/packages
-mime_DATA = src/ui/gui/pspp.xml
-EXTRA_DIST += src/ui/gui/pspp.xml
 
 desktopdir = $(datadir)/applications
 desktop_DATA = src/ui/gui/org.fsf.pspp.desktop

src/ui/gui/pspp.xml
@@ -1,16 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
-  <mime-type type="application/x-spss-sps">
-    <comment>SPSS syntax file</comment>
-    <acronym>SPSS</acronym>
-    <expanded-acronym>Statistical Package for the Social Sciences</expanded-acronym>
-    <glob pattern="*.sps"/>
-  </mime-type>
-  <mime-type type="application/x-spss-spv">
-    <comment>SPSS output file</comment>
-    <acronym>SPSS</acronym>
-    <expanded-acronym>Statistical Package for the Social Sciences</expanded-acronym>
-    <glob pattern="*.spv"/>
-  </mime-type>
-</mime-info>
-

commit 9a626036211e61388a4d255051e1774d01bbe072

Restore the mime defintion file pspp.xml
Commit 8ec823cac8dd9eee5d0ad6cee9d001b9fdb673ba inadvertently
removed pspp.xml - the mime definition file.  This change
restores it and also takes it out of the gui definition directory.

Makefile.am
@@ -163,3 +163,10 @@ PHONY += dist-hook-git dummy-prerequisistes
 -include dummy-prerequisistes
 
 dummy-prerequisistes: $(BUILT_SOURCES) gl/libgl.la
+
+
+
+mimedir = $(datadir)/mime/packages
+mime_DATA =pspp.xml
+EXTRA_DIST += pspp.xml
+

doc/automake.mk
@@ -181,7 +181,6 @@ install-html-local: html-local
 
 
 
-mimedir = $(datadir)/mime/packages
 desktopdir = $(datadir)/applications
 
 doc/org.fsf.pspp.metainfo.xml: doc/org.fsf.pspp.metainfo.xml.in $(POFILES)

pspp.xml
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="application/x-spss-sps">
+    <comment>SPSS syntax file</comment>
+    <acronym>SPSS</acronym>
+    <expanded-acronym>Statistical Package for the Social Sciences</expanded-acronym>
+    <glob pattern="*.sps"/>
+  </mime-type>
+  <mime-type type="application/x-spss-spv">
+    <comment>SPSS output file</comment>
+    <acronym>SPSS</acronym>
+    <expanded-acronym>Statistical Package for the Social Sciences</expanded-acronym>
+    <glob pattern="*.spv"/>
+  </mime-type>
+</mime-info>
+