lemenkov/pspp

commit 507ebaea36737618ef8265a60cd3e9005d9f3457

output: Make groups contain their subitems, and get rid of spv_item.
Until now, the output subsystem has had two different ideas for output
items: struct output_item, which is the primary representation, and
struct spv_item, which represents an output item read from an .spv file.
The biggest difference, until now, has been that spv_item contains its
children, whereas output_item bracketed children inside open/close pairs.
This commit unifies them under output_item, making output_item adopt the
container abstraction.

src/ui/gui/psppire-output-view.c
@@ -325,7 +325,7 @@ rerender (struct psppire_output_view *view)
       if (view->y > 0)
         view->y += view->object_spacing;
 
-      if (item->item->type == OUTPUT_ITEM_GROUP_OPEN)
+      if (item->item->type == OUTPUT_ITEM_GROUP)
         continue;
 
       r = xr_fsm_create_for_scrolling (item->item, view->style, cr);
@@ -396,19 +396,10 @@ psppire_output_view_put (struct psppire_output_view *view,
   GtkWidget *drawing_area;
   int tw, th;
 
-  if (item->type == OUTPUT_ITEM_GROUP_CLOSE)
-    {
-      if (view->cur_group)
-        {
-          if (!gtk_tree_path_up (view->cur_group))
-            {
-              gtk_tree_path_free (view->cur_group);
-              view->cur_group = NULL;
-            }
-        }
-      return;
-    }
-  else if (item->type == OUTPUT_ITEM_TEXT)
+  if (item->type == OUTPUT_ITEM_GROUP)
+    return;
+
+  if (item->type == OUTPUT_ITEM_TEXT)
     {
       char *text = text_item_get_plain_text (item);
       bool text_is_empty = text[0] == '\0';
@@ -425,9 +416,7 @@ psppire_output_view_put (struct psppire_output_view *view,
   view_item->drawing_area = NULL;
 
   GdkWindow *win = gtk_widget_get_window (GTK_WIDGET (view->output));
-  if (item->type == OUTPUT_ITEM_GROUP_OPEN)
-    tw = th = 0;
-  else if (win)
+  if (win)
     {
       view_item->drawing_area = drawing_area = gtk_drawing_area_new ();
 
@@ -473,13 +462,7 @@ psppire_output_view_put (struct psppire_output_view *view,
       else
         gtk_tree_store_append (store, &iter, NULL);
 
-      if (item->type == OUTPUT_ITEM_GROUP_OPEN)
-        {
-          gtk_tree_path_free (view->cur_group);
-          view->cur_group = gtk_tree_model_get_path (GTK_TREE_MODEL (store),
-                                                     &iter);
-        }
-
+      /* XXX group? */
       gtk_tree_store_set (store, &iter,
                           COL_LABEL, output_item_get_label (item),
 			  COL_ADDR, item,
@@ -1102,10 +1085,10 @@ psppire_output_view_submit (struct output_driver *this,
 
 static struct output_driver_class psppire_output_view_driver_class =
   {
-    "PSPPIRE Output View",      /* name */
-    NULL,                       /* destroy */
-    psppire_output_view_submit, /* submit */
-    NULL,                       /* flush */
+    .name = "PSPPIRE Output View",
+    .submit = psppire_output_view_submit,
+    .handles_groups = true,
+    .handles_show = true,
   };
 
 void

src/language/command.c
@@ -36,6 +36,7 @@
 #include "libpspp/i18n.h"
 #include "libpspp/message.h"
 #include "libpspp/str.h"
+#include "output/driver.h"
 #include "output/output-item.h"
 
 #include "xmalloca.h"
@@ -173,8 +174,8 @@ do_parse_command (struct lexer *lexer,
 		  struct dataset *ds, enum cmd_state state)
 {
   const struct command *command = NULL;
+  size_t nesting_level = SIZE_MAX;
   enum cmd_result result;
-  bool opened = false;
   int n_tokens;
 
   /* Read the command's first token. */
@@ -198,10 +199,10 @@ do_parse_command (struct lexer *lexer,
       result = CMD_FAILURE;
       goto finish;
     }
-  output_item_submit (group_open_item_create_nocopy (
-                        utf8_to_title (command->name),
-                        utf8_to_title (command->name)));
-  opened = true;
+
+  nesting_level = output_open_group (group_item_create_nocopy (
+                                       utf8_to_title (command->name),
+                                       utf8_to_title (command->name)));
 
   if (command->function == NULL)
     {
@@ -247,8 +248,8 @@ finish:
     while (lex_token (lexer) == T_ENDCMD)
       lex_get (lexer);
 
-  if (opened)
-    output_item_submit (group_close_item_create ());
+  if (nesting_level != SIZE_MAX)
+    output_close_groups (nesting_level);
 
   return result;
 }

src/output/ascii.c
@@ -584,11 +584,9 @@ ascii_output_table_item_unref (struct ascii_driver *a,
 }
 
 static void
-ascii_submit (struct output_driver *driver,
-              const struct output_item *item)
+ascii_submit (struct output_driver *driver, const struct output_item *item)
 {
   struct ascii_driver *a = ascii_driver_cast (driver);
-
   if (a->error)
     return;
 
@@ -620,9 +618,8 @@ ascii_submit (struct output_driver *driver,
     case OUTPUT_ITEM_CHART:
       if (a->chart_file_name != NULL)
         {
-          char *file_name = xr_draw_png_chart (item->chart, a->chart_file_name,
-                                               ++a->chart_cnt, &a->fg,
-                                               &a->bg);
+          char *file_name = xr_draw_png_chart (
+            item->chart, a->chart_file_name, ++a->chart_cnt, &a->fg, &a->bg);
           if (file_name != NULL)
             {
               struct output_item *text_item = text_item_create_nocopy (
@@ -647,11 +644,12 @@ ascii_submit (struct output_driver *driver,
       ascii_output_table_item_unref (
         a, text_item_to_table_item (
           message_item_to_text_item (
-              output_item_ref (item))));
+            output_item_ref (item))));
       break;
 
-    case OUTPUT_ITEM_GROUP_OPEN:
-    case OUTPUT_ITEM_GROUP_CLOSE:
+    case OUTPUT_ITEM_GROUP:
+      NOT_REACHED ();
+
     case OUTPUT_ITEM_PAGE_BREAK:
     case OUTPUT_ITEM_PAGE_SETUP:
       break;
@@ -665,10 +663,10 @@ const struct output_driver_factory list_driver_factory =
 
 static const struct output_driver_class ascii_driver_class =
   {
-    "text",
-    ascii_destroy,
-    ascii_submit,
-    ascii_flush,
+    .name = "text",
+    .destroy = ascii_destroy,
+    .submit = ascii_submit,
+    .flush = ascii_flush,
   };
 
 static char *ascii_reserve (struct ascii_driver *, int y, int x0, int x1,

src/output/automake.mk
@@ -84,6 +84,8 @@ src_output_liboutput_la_SOURCES = \
 	src/output/pivot-table.h \
 	src/output/render.c \
 	src/output/render.h \
+	src/output/select.c \
+	src/output/select.h \
 	src/output/spv-driver.c \
 	src/output/table-provider.h \
 	src/output/table.c \

src/output/cairo-fsm.c
@@ -990,8 +990,7 @@ xr_fsm_create (const struct output_item *item_,
       item = output_item_ref (item_);
       break;
 
-    case OUTPUT_ITEM_GROUP_OPEN:
-    case OUTPUT_ITEM_GROUP_CLOSE:
+    case OUTPUT_ITEM_GROUP:
     case OUTPUT_ITEM_PAGE_SETUP:
       return NULL;
 
@@ -1148,8 +1147,7 @@ xr_fsm_measure (struct xr_fsm *fsm, cairo_t *cr, int *wp, int *hp)
       fsm->cairo = NULL;
       break;
 
-    case OUTPUT_ITEM_GROUP_OPEN:
-    case OUTPUT_ITEM_GROUP_CLOSE:
+    case OUTPUT_ITEM_GROUP:
     case OUTPUT_ITEM_MESSAGE:
     case OUTPUT_ITEM_PAGE_BREAK:
     case OUTPUT_ITEM_PAGE_SETUP:
@@ -1213,8 +1211,7 @@ xr_fsm_draw_region (struct xr_fsm *fsm, cairo_t *cr,
       fsm->cairo = NULL;
       break;
 
-    case OUTPUT_ITEM_GROUP_OPEN:
-    case OUTPUT_ITEM_GROUP_CLOSE:
+    case OUTPUT_ITEM_GROUP:
     case OUTPUT_ITEM_MESSAGE:
     case OUTPUT_ITEM_PAGE_BREAK:
     case OUTPUT_ITEM_PAGE_SETUP:
@@ -1346,8 +1343,7 @@ xr_fsm_draw_slice (struct xr_fsm *fsm, cairo_t *cr, int space)
       used = xr_fsm_draw_table (fsm, space);
       break;
 
-    case OUTPUT_ITEM_GROUP_OPEN:
-    case OUTPUT_ITEM_GROUP_CLOSE:
+    case OUTPUT_ITEM_GROUP:
     case OUTPUT_ITEM_MESSAGE:
     case OUTPUT_ITEM_PAGE_SETUP:
     case OUTPUT_ITEM_TEXT:

src/output/cairo-pager.c
@@ -101,20 +101,19 @@ struct xr_pager
 
     /* Current output item. */
     struct xr_fsm *fsm;
-    struct output_item *item;
+    struct output_iterator iter;
+    struct output_item *root_item;
     int slice_idx;
+    const char *label;
 
-    /* Grouping, for constructing the outline for PDFs.
-
-       The 'group_ids' were returned by cairo_pdf_surface_add_outline() and
-       represent the groups within which upcoming output is nested.  The
-       'group_opens' will be passed to cairo_pdf_surface_add_outline() when the
-       next item is rendered (we defer it so that the location associated with
-       the outline item can be the first object actually output in it). */
-    int *group_ids;
-    size_t n_group_ids, allocated_group_ids;
-    struct output_item **group_opens;
-    size_t n_opens, allocated_opens;
+    /* Grouping, for constructing the outline for PDFs. */
+    struct outline_node
+      {
+        const struct output_item *item;
+        int group_id;
+      }
+    *nodes;
+    size_t n_nodes, allocated_nodes;
 
     /* Current output page. */
     cairo_t *cr;
@@ -248,16 +247,14 @@ xr_pager_destroy (struct xr_pager *p)
 {
   if (p)
     {
-      free (p->group_ids);
-      for (size_t i = 0; i < p->n_opens; i++)
-        output_item_unref (p->group_opens[i]);
-      free (p->group_opens);
+      free (p->nodes);
 
       xr_page_style_unref (p->page_style);
       xr_fsm_style_unref (p->fsm_style);
 
       xr_fsm_destroy (p->fsm);
-      output_item_unref (p->item);
+      output_iterator_destroy (&p->iter);
+      output_item_unref (p->root_item);
 
       if (p->cr)
         {
@@ -271,15 +268,15 @@ xr_pager_destroy (struct xr_pager *p)
 bool
 xr_pager_has_item (const struct xr_pager *p)
 {
-  return p->item != NULL;
+  return p->root_item != NULL;
 }
 
 void
 xr_pager_add_item (struct xr_pager *p, const struct output_item *item)
 {
-  assert (!p->item);
-  p->item = output_item_ref (item);
-  p->slice_idx = 0;
+  assert (!p->root_item);
+  p->root_item = output_item_ref (item);
+  output_iterator_init (&p->iter, item);
   xr_pager_run (p);
 }
 
@@ -339,7 +336,7 @@ xr_pager_finish_page (struct xr_pager *p)
 bool
 xr_pager_needs_new_page (struct xr_pager *p)
 {
-  if (p->item && (!p->cr || p->y >= p->fsm_style->size[V]))
+  if (p->root_item && (!p->cr || p->y >= p->fsm_style->size[V]))
     {
       xr_pager_finish_page (p);
       return true;
@@ -363,106 +360,104 @@ add_outline (cairo_t *cr, int parent_id,
 static void
 xr_pager_run (struct xr_pager *p)
 {
-  if (p->item && p->cr && p->y < p->fsm_style->size[V])
+  if (!p->root_item || !p->cr || p->y >= p->fsm_style->size[V])
+    return;
+
+  for (;;)
     {
-      if (!p->fsm)
+      /* Make sure we've got an object to render. */
+      while (!p->fsm)
         {
-          if (p->item->type == OUTPUT_ITEM_GROUP_OPEN)
+          /* If there are no remaining objects to render, then we're done. */
+          if (!p->iter.cur)
             {
-              if (p->n_opens >= p->allocated_opens)
-                p->group_opens = x2nrealloc (p->group_opens,
-                                             &p->allocated_opens,
-                                             sizeof p->group_opens);
-              p->group_opens[p->n_opens++] = output_item_ref (p->item);
-            }
-          else if (p->item->type == OUTPUT_ITEM_GROUP_CLOSE)
-            {
-              if (p->n_opens)
-                output_item_unref (p->group_opens[--p->n_opens]);
-              else if (p->n_group_ids)
-                p->n_group_ids--;
-              else
-                {
-                  /* Something wrong! */
-                }
-            }
-
-          p->fsm = xr_fsm_create_for_printing (p->item, p->fsm_style, p->cr);
-          if (!p->fsm)
-            {
-              output_item_unref (p->item);
-              p->item = NULL;
-
+              output_item_unref (p->root_item);
+              p->root_item = NULL;
               return;
             }
+
+          /* Prepare to render the current object. */
+          p->fsm = xr_fsm_create_for_printing (p->iter.cur, p->fsm_style,
+                                               p->cr);
+          p->label = output_item_get_label (p->iter.cur);
+          p->slice_idx = 0;
+          while (p->n_nodes > p->iter.n)
+            p->n_nodes--;
+          while (p->n_nodes)
+            {
+              size_t i = p->n_nodes - 1;
+              if (p->nodes[i].item == p->iter.nodes[i].group)
+                break;
+
+              p->nodes--;
+            }
+          while (p->n_nodes < p->iter.n)
+            {
+              if (p->n_nodes >= p->allocated_nodes)
+                p->nodes = x2nrealloc (p->nodes, &p->allocated_nodes,
+                                       sizeof *p->nodes);
+              size_t i = p->n_nodes++;
+              p->nodes[i] = (struct outline_node) {
+                .item = p->iter.nodes[i].group,
+              };
+            }
+          output_iterator_next (&p->iter);
         }
 
-      for (;;)
+      char *dest_name = NULL;
+      if (p->page_style->include_outline)
         {
-          char *dest_name = NULL;
-          if (p->page_style->include_outline)
+          static int counter = 0;
+          dest_name = xasprintf ("dest%d", counter++);
+          char *attrs = xasprintf ("name='%s'", dest_name);
+          cairo_tag_begin (p->cr, CAIRO_TAG_DEST, attrs);
+          free (attrs);
+        }
+
+      int spacing = p->fsm_style->object_spacing;
+      int chunk = xr_fsm_draw_slice (p->fsm, p->cr,
+                                     p->fsm_style->size[V] - p->y);
+      p->y += chunk + spacing;
+      cairo_translate (p->cr, 0, xr_to_pt (chunk + spacing));
+
+      if (p->page_style->include_outline)
+        {
+          cairo_tag_end (p->cr, CAIRO_TAG_DEST);
+
+          if (chunk && p->slice_idx++ == 0)
             {
-              static int counter = 0;
-              dest_name = xasprintf ("dest%d", counter++);
-              char *attrs = xasprintf ("name='%s'", dest_name);
-              cairo_tag_begin (p->cr, CAIRO_TAG_DEST, attrs);
+              char *attrs = xasprintf ("dest='%s'", dest_name);
+
+              int parent_group_id = CAIRO_PDF_OUTLINE_ROOT;
+              for (size_t i = 0; i < p->n_nodes; i++)
+                {
+                  struct outline_node *node = &p->nodes[i];
+                  if (!node->group_id)
+                    {
+                      const char *label = output_item_get_label (node->item);
+                      node->group_id = add_outline (
+                        p->cr, parent_group_id, label, attrs,
+                        CAIRO_PDF_OUTLINE_FLAG_OPEN);
+                    }
+                  parent_group_id = node->group_id;
+                }
+
+              add_outline (p->cr, parent_group_id, p->label, attrs, 0);
               free (attrs);
             }
+          free (dest_name);
+        }
 
-          int spacing = p->fsm_style->object_spacing;
-          int chunk = xr_fsm_draw_slice (p->fsm, p->cr,
-                                         p->fsm_style->size[V] - p->y);
-          p->y += chunk + spacing;
-          cairo_translate (p->cr, 0, xr_to_pt (chunk + spacing));
-
-          if (p->page_style->include_outline)
-            {
-              cairo_tag_end (p->cr, CAIRO_TAG_DEST);
-
-              if (chunk && p->slice_idx++ == 0)
-                {
-                  char *attrs = xasprintf ("dest='%s'", dest_name);
-
-                  int parent_group_id = (p->n_group_ids
-                                         ? p->group_ids[p->n_group_ids - 1]
-                                         : CAIRO_PDF_OUTLINE_ROOT);
-                  for (size_t i = 0; i < p->n_opens; i++)
-                    {
-                      parent_group_id = add_outline (
-                        p->cr, parent_group_id,
-                        output_item_get_label (p->group_opens[i]),
-                        attrs, CAIRO_PDF_OUTLINE_FLAG_OPEN);
-                      output_item_unref (p->group_opens[i]);
-
-                      if (p->n_group_ids >= p->allocated_group_ids)
-                        p->group_ids = x2nrealloc (p->group_ids,
-                                                   &p->allocated_group_ids,
-                                                   sizeof *p->group_ids);
-                      p->group_ids[p->n_group_ids++] = parent_group_id;
-                    }
-                  p->n_opens = 0;
-
-                  add_outline (p->cr, parent_group_id,
-                               output_item_get_label (p->item), attrs, 0);
-                  free (attrs);
-                }
-              free (dest_name);
-            }
-
-          if (xr_fsm_is_empty (p->fsm))
-            {
-              xr_fsm_destroy (p->fsm);
-              p->fsm = NULL;
-              output_item_unref (p->item);
-              p->item = NULL;
-              return;
-            }
-          else if (!chunk)
-            {
-              assert (p->y > 0);
-              p->y = INT_MAX;
-              return;
-            }
+      if (xr_fsm_is_empty (p->fsm))
+        {
+          xr_fsm_destroy (p->fsm);
+          p->fsm = NULL;
+        }
+      else if (!chunk)
+        {
+          assert (p->y > 0);
+          p->y = INT_MAX;
+          return;
         }
     }
 }

src/output/cairo.c
@@ -639,8 +639,8 @@ struct output_driver_factory png_driver_factory =
 
 static const struct output_driver_class cairo_driver_class =
 {
-  "cairo",
-  xr_destroy,
-  xr_submit,
-  NULL,
+  .name = "cairo",
+  .destroy = xr_destroy,
+  .submit = xr_submit,
+  .handles_groups = true,
 };

src/output/csv.c
@@ -245,11 +245,8 @@ csv_submit (struct output_driver *driver,
     case OUTPUT_ITEM_CHART:
       break;
 
-    case OUTPUT_ITEM_GROUP_OPEN:
-      break;
-
-    case OUTPUT_ITEM_GROUP_CLOSE:
-      break;
+    case OUTPUT_ITEM_GROUP:
+      NOT_REACHED ();
 
     case OUTPUT_ITEM_IMAGE:
       break;
@@ -296,8 +293,8 @@ struct output_driver_factory csv_driver_factory = { "csv", "-", csv_create };
 
 static const struct output_driver_class csv_driver_class =
   {
-    "csv",
-    csv_destroy,
-    csv_submit,
-    csv_flush,
+    .name = "csv",
+    .destroy = csv_destroy,
+    .submit = csv_submit,
+    .flush = csv_flush,
   };

commit cb4ae645c499263005ea52d8ee8f09f11da232c3

psppire-output-view: Fix display of groups.
Bug introduced in commit 507ebaea3673 ("output: Make groups contain
their subitems, and get rid of spv_item.").

Reported by John Darrington.

src/ui/gui/psppire-output-view.c
@@ -43,6 +43,7 @@ struct output_view_item
   {
     struct output_item *item;
     GtkWidget *drawing_area;
+    int width, height;
   };
 
 struct psppire_output_view
@@ -56,7 +57,6 @@ struct psppire_output_view
     glong y;
 
     GtkTreeView *overview;
-    GtkTreePath *cur_group;
 
     GtkWidget *toplevel;
 
@@ -387,39 +387,16 @@ rerender (struct psppire_output_view *view)
   cairo_region_destroy (region);
 }
 
-
-void
-psppire_output_view_put (struct psppire_output_view *view,
-                         const struct output_item *item)
+static bool
+init_output_view_item (struct output_view_item *view_item,
+                       struct psppire_output_view *view,
+                       const struct output_item *item)
 {
-  struct output_view_item *view_item;
-  GtkWidget *drawing_area;
-  int tw, th;
-
-  if (item->type == OUTPUT_ITEM_GROUP)
-    return;
-
-  if (item->type == OUTPUT_ITEM_TEXT)
-    {
-      char *text = text_item_get_plain_text (item);
-      bool text_is_empty = text[0] == '\0';
-      free (text);
-      if (text_is_empty)
-        return;
-    }
-
-  if (view->n_items >= view->allocated_items)
-    view->items = x2nrealloc (view->items, &view->allocated_items,
-                                sizeof *view->items);
-  view_item = &view->items[view->n_items++];
-  view_item->item = output_item_ref (item);
-  view_item->drawing_area = NULL;
+  *view_item = (struct output_view_item) { .item = output_item_ref (item) };
 
   GdkWindow *win = gtk_widget_get_window (GTK_WIDGET (view->output));
-  if (win)
+  if (win && item->type != OUTPUT_ITEM_GROUP)
     {
-      view_item->drawing_area = drawing_area = gtk_drawing_area_new ();
-
       if (!view->style)
         view->style = get_xr_fsm_style (view);
 
@@ -435,17 +412,45 @@ psppire_output_view_put (struct psppire_output_view *view,
 	{
 	  gdk_window_end_draw_frame (win, ctx);
 	  cairo_region_destroy (region);
-	  return;
+
+          output_item_unref (view_item->item);
+	  return false;
 	}
 
-      xr_fsm_measure (r, cr, &tw, &th);
-      create_drawing_area (view, drawing_area, r, tw, th, item);
+      xr_fsm_measure (r, cr, &view_item->width, &view_item->height);
+      view_item->drawing_area = gtk_drawing_area_new ();
+      create_drawing_area (view, view_item->drawing_area, r, view_item->width,
+                           view_item->height, item);
       gdk_window_end_draw_frame (win, ctx);
       cairo_region_destroy (region);
     }
-  else
-    tw = th = 0;
 
+  return true;
+}
+
+static void
+psppire_output_view_put__ (struct psppire_output_view *view,
+                           const struct output_item *item,
+                           GtkTreePath *parent_path)
+{
+  if (item->type == OUTPUT_ITEM_TEXT)
+    {
+      char *text = text_item_get_plain_text (item);
+      bool text_is_empty = text[0] == '\0';
+      free (text);
+      if (text_is_empty)
+        return;
+    }
+
+  if (view->n_items >= view->allocated_items)
+    view->items = x2nrealloc (view->items, &view->allocated_items,
+                              sizeof *view->items);
+  struct output_view_item *view_item = &view->items[view->n_items];
+  if (!init_output_view_item (view_item, view, item))
+    return;
+  view->n_items++;
+
+  GtkTreePath *path = NULL;
   if (view->overview)
     {
       GtkTreeStore *store = GTK_TREE_STORE (
@@ -454,32 +459,44 @@ psppire_output_view_put (struct psppire_output_view *view,
       /* Create a new node in the tree and puts a reference to it in 'iter'. */
       GtkTreeIter iter;
       GtkTreeIter parent;
-      if (view->cur_group
-          && gtk_tree_path_get_depth (view->cur_group) > 0
+      if (parent_path
+          && gtk_tree_path_get_depth (parent_path) > 0
           && gtk_tree_model_get_iter (GTK_TREE_MODEL (store),
-                                      &parent, view->cur_group))
+                                      &parent, parent_path))
         gtk_tree_store_append (store, &iter, &parent);
       else
         gtk_tree_store_append (store, &iter, NULL);
 
-      /* XXX group? */
       gtk_tree_store_set (store, &iter,
                           COL_LABEL, output_item_get_label (item),
 			  COL_ADDR, item,
                           COL_Y, view->y,
                           -1);
 
-      GtkTreePath *path = gtk_tree_model_get_path (
+      /* Get the path of the new row. */
+      path = gtk_tree_model_get_path (
         GTK_TREE_MODEL (store), &iter);
       gtk_tree_view_expand_row (view->overview, path, TRUE);
-      gtk_tree_path_free (path);
     }
 
-  if (view->max_width < tw)
-    view->max_width = tw;
-  view->y += th;
+  if (view->max_width < view_item->width)
+    view->max_width = view_item->width;
+  view->y += view_item->height;
 
   gtk_layout_set_size (view->output, view->max_width, view->y);
+
+  if (item->type == OUTPUT_ITEM_GROUP)
+    for (size_t i = 0; i < item->group.n_children; i++)
+      psppire_output_view_put__ (view, item->group.children[i], path);
+
+  gtk_tree_path_free (path);
+}
+
+void
+psppire_output_view_put (struct psppire_output_view *view,
+                         const struct output_item *item)
+{
+  psppire_output_view_put__ (view, item, NULL);
 }
 
 static void
@@ -852,9 +869,6 @@ psppire_output_view_destroy (struct psppire_output_view *view)
 
   if (view->print_settings != NULL)
     g_object_unref (view->print_settings);
-
-  if (view->cur_group)
-    gtk_tree_path_free (view->cur_group);
 
   free (view);
 }